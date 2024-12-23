134aca4a0cad410e242c53edce61cd503c3670a3
Shreenidhi Sudhakar <shree6791@yahoo.com>
2024-12-23 16:05:18 +0000

    support deployment on netlify

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-redirected',
+            value: '(?!true)',
+          },
+        ],
+        destination: '/:path*',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
