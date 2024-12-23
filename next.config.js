e8ff37c398362439d3b94fc3ff86cf8ffe22c068
Shreenidhi Sudhakar <shree6791@yahoo.com>
2024-12-23 16:21:14 +0000

    fix build issue - 3

--- next.config.js
+++ next.config.js
@@ -1,26 +1,36 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-redirected',
-            value: '(?!true)',
-          },
-        ],
-        destination: '/:path*',
-        permanent: false,
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+    reactStrictMode: true,
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'header',
+              key: 'x-redirected',
+              value: '(?!true)',
+            },
+          ],
+          destination: '/:path*',
+          permanent: false,
+        },
+      ];
+    },
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, './'),
+      }
+      return config
+    },
+  };
+  
+  module.exports = nextConfig;
+  
