f1c9b5f0f57d001738e15be82833317553011310
Shreenidhi Sudhakar <shree6791@yahoo.com>
2024-12-24 00:47:09 +0000

    update header to team from leadershio

--- components/navigation/nav-links.tsx
+++ components/navigation/nav-links.tsx
@@ -1,6 +1,6 @@
 export const navLinks = [
   { href: '/about', label: 'About Us' },
-  { href: '/leadership', label: 'Leadership' },
+  { href: '/team', label: 'Team' },
   { href: '/faqs', label: 'FAQs' },
   { href: '/contact', label: 'Contact Us' },
 ] as const;
